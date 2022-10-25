0012d0c37011a1e70e1f5acf3efe22a64e8ac86b
Kimjunghwoi <poiuy5947@gmail.coml>
2022-10-25 17:01:51 +0900

    feat: NAVER Login, README
    1. Naver Login
    2. README.md

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 스프링 부트와 AWS로 혼자 구현하는 웹 서비스
 ---
+<br/>
+<br/>
+
+---
+
+<div align=center><h1>📚 STACKS</h1></div>
+<br/>
+
+---
+<br/>
+<div align=left> 
+  <img src="https://img.shields.io/badge/java-007396?style=for-the-badge&logo=java&logoColor=white"> 
+  <br>
+
+  <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"> 
+  <img src="https://img.shields.io/badge/jquery-0769AD?style=for-the-badge&logo=jquery&logoColor=white">
+  <img src="https://img.shields.io/badge/mustache-ED2761?style=for-the-badge&logo=Teespring&logoColor=white">
+  <br>
+
+  <img src="https://img.shields.io/badge/spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white">
+  <img src="https://img.shields.io/badge/springboot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white">
+  <img src="https://img.shields.io/badge/springsecurity-6DB33F?style=for-the-badge&logo=springsecurity&logoColor=white">
+  <img src="https://img.shields.io/badge/oauth2.0-6DB33F?style=for-the-badge&logo=Aircall&logoColor=white">
+  <br>
+  
+  <img src="https://img.shields.io/badge/bootstrap-7952B3?style=for-the-badge&logo=bootstrap&logoColor=white">
+  <br>
+
+[//]: # (  <img src="https://img.shields.io/badge/linux-FCC624?style=for-the-badge&logo=linux&logoColor=black"> )
+  <img src="https://img.shields.io/badge/amazonaws-232F3E?style=for-the-badge&logo=amazonaws&logoColor=white"> 
+
+[//]: # (  <img src="https://img.shields.io/badge/apache tomcat-F8DC75?style=for-the-badge&logo=apachetomcat&logoColor=white">)
+  <br>
+
+  <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
+  <img src="https://img.shields.io/badge/git-F05032?style=for-the-badge&logo=git&logoColor=white">
+
+[//]: # (  <img src="https://img.shields.io/badge/fontawesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white">)
+  <br>
+</div>
 
 
-
-
-
-
-[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=kimjunghwoi)](https://github.com/kimjunghwoi/github-readme-stats)
+[//]: # ()
+[//]: # ()
+[//]: # ([![Anurag's GitHub stats]&#40;https://github-readme-stats.vercel.app/api?username=kimjunghwoi&#41;]&#40;https://github.com/kimjunghwoi/github-readme-stats&#41;)
--- src/test/java/com/example/springbootwebservice/web/PostsApiControllerTest.java
+++ src/test/java/com/example/springbootwebservice/web/PostsApiControllerTest.java
@@ -4,29 +4,29 @@ import com.example.springbootwebservice.web.domain.posts.Posts;
 import com.example.springbootwebservice.web.domain.posts.PostsRepository;
 import com.example.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.example.springbootwebservice.web.dto.PostsUpdateRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-/*
-    @WebMvcTest의 경우 JPA 기능이 작동하지 않기 때문에 사용하지 않았다.
-    @SpringBootTest와 TestRestTemplate 사용
-    SpringBootTest.WebEnvironment.RANDOM_PORT 랜덤 포트 사용
-*/
+import static  org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,8 +38,22 @@ public class PostsApiControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    // 추가 MockMvc
     @Autowired
     private PostsRepository postsRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before // 매번 테스트가 시작되기전에 MockMvc 인스턴스를 생성합니다.
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -47,6 +61,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -60,75 +75,51 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        // mvc로 수정
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception {
         //given
-        Posts savePosts = postsRepository.save(Posts.builder()
+        Posts savedPosts = postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
                 .author("author")
                 .build());
-        System.out.println("================================================= 1");
-        Long updateId = savePosts.getId();
+
+        Long updateId = savedPosts.getId();
         String expectedTitle = "title2";
         String expectedContent = "content2";
+
         PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
                 .title(expectedTitle)
                 .content(expectedContent)
                 .build();
-        System.out.println("================================================= 2");
+
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-        System.out.println("================================================= 3");
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-        System.out.println("================================================= 4");
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+//        mvc.perform
+//        생성된 MockMvc를 통해 API를 테스트 합니다.
+//        본문(Body) 영역은 문자열로 표현하기 위해 ObjectMapper를 통해 문자열 JSON으로 변환합니다.
+
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-        System.out.println("================================================= 5");
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-        System.out.println("================================================= 6");
     }
-
-    @Test
-    public void BaseTimeEntity_등록() {
-        //given
-        LocalDateTime now = LocalDateTime.of(2020, 3,17,0,0,0);
-        postsRepository.save(Posts.builder()
-                .title("title")
-                .content("content")
-                .author("author")
-                .build());
-
-        //when
-        List<Posts> postsList = postsRepository.findAll();
-
-        //then
-        Posts posts = postsList.get(0);
-
-        System.out.println(">>>>>>>>>>> createDate="+posts.getCreateDate()
-                +", modeifeidDate="+posts.getModifiedDate());
-
-
-        assertThat(posts.getCreateDate()).isAfter(now);
-        assertThat(posts.getModifiedDate()).isAfter(now);
-
-    }
-
-
 }
