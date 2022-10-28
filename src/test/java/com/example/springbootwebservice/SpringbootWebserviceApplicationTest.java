9238f942fb25a7d6c6d3f0a663fe44866ab05d3b
Kimjunghwoi <poiuy5947@gmail.coml>
2022-10-28 10:22:54 +0900

    feat: ec2 jdbc , properties setting

---
+++ src/test/java/com/example/springbootwebservice/SpringbootWebserviceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpringbootWebserviceApplicationTest {
+
+    @Test
+    public void main() {
+    }
+}
